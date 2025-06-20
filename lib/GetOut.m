0d4588ea09ac26ba70b3de207c769cf4ae19ee44
LLX-SHERRY <168443938+LLX-SHERRY@users.noreply.github.com>
2025-06-20 22:43:52 +0800

    Delete lib directory

--- lib/GetOut.m
+++
@@ -1,5 +0,0 @@
-global Out;
-global bhr6;
-if(bhr6.StateFlag ~=0)
-Out = CrawlUpdate(bhr6.BasePosture,bhr6.JointAngle,bhr6.Inter_Time*1000);
-end
--- lib/Read_Joint.m
+++
@@ -1,6 +0,0 @@
-function [ joint ] = Read_Joint( data_path )
-%READ_JOINT 从RTX文件转化到VREP关节角度
-%   Detailed explanation goes here
-    data = load(data_path);
-    joint = [data(:,2:7) data(:,9:14) data(:,16:18) data(:,21:22) data(:,24) data(:,26:27) data(:,29)];
-end
